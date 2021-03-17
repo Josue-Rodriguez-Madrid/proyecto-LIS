a27e2689ea3dc25544cc489d274775aca33b1ff7
Josue-Rodriguez-Madrid <josuemadrid.cat@gmail.com>
2021-03-17 10:12:25 -0600

    galeria.php

---
+++ public/equipo.php
@@ -0,0 +1,22 @@
+<?php
+include("../master/estilos.php");
+include("../master/navbar.php");
+?>
+<html>
+<head>
+<meta charset="utf-8"/><!-- tipo de escritura para que acepte caracteres especiales -->
+<title> Nuestro Equipo | 2021 </title>
+</head>
+<body>
+<!-- Contenido de la pagina -->
+
+<!-- Fin del contenido de la pagina -->
+<?php
+include("../master/footer.php");
+?>
+<?php
+include("../master/scripts.php");
+?>
+</body>
+</body>
+</html>
